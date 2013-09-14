97a0bdcef69ca69db74cfeb3129b2df9736b83b1
Adam Stankiewicz <sheerun@sher.pl>
2013-09-14 20:46:47 +0200

    fix: Build spec, use bash, check return status

--- spec/build_spec.rb
+++ spec/build_spec.rb
@@ -3,6 +3,6 @@ $plugin_path = File.expand_path('../..', __FILE__)
 describe 'build script' do
   it 'should run and return success code' do
     Dir.chdir($plugin_path)
-    system('./build')
+    expect(system('bash ./build')).to be_true
   end
 end
