77f091c8c7ab6dbd1d1edc778a8da7ff62731f6b
Adam Stankiewicz <sheerun@sher.pl>
2013-09-14 20:09:32 +0200

    Add specs to be sure all plugins are loading properly

---
+++ spec/spec/loading_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "My Vim plugin" do
+  languages = Dir["#{PLUGIN_PATH}/syntax/*.vim"].map { |f| f.split('/').last.gsub('.vim', '') }
+
+  languages.each do |lang|
+    it "should parse .#{lang} file" do
+      write_file "test.#{lang}", ""
+      vim.edit "test.#{lang}"
+      vim.insert "sample"
+      vim.write
+    end
+  end
+end
---
+++ spec/spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'vimrunner'
+require 'vimrunner/rspec'
+
+PLUGIN_PATH = File.expand_path('../../..', __FILE__)
+puts PLUGIN_PATH
+
+Vimrunner::RSpec.configure do |config|
+  # Use a single Vim instance for the test suite. Set to false to use an
+  # instance per test (slower, but can be easier to manage).
+  config.reuse_server = true
+
+  # Decide how to start a Vim instance. In this block, an instance should be
+  # spawned and set up with anything project-specific.
+  config.start_vim do
+    vim = Vimrunner.start
+
+    # Setup your plugin in the Vim instance
+    vim.add_plugin(PLUGIN_PATH)
+
+    # The returned value is the Client available in the tests.
+    vim
+  end
+end
