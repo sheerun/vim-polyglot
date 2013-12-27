a0c5f3ee5593760d92791c615540658108dbdfe8
Adam Stankiewicz <sheerun@sher.pl>
2013-12-27 16:40:01 +0100

    Try to not reuse server for Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ $plugin_path = File.expand_path('../..', __FILE__)
 Vimrunner::RSpec.configure do |config|
   # Use a single Vim instance for the test suite. Set to false to use an
   # instance per test (slower, but can be easier to manage).
-  config.reuse_server = true
+  config.reuse_server = false
 
   # Decide how to start a Vim instance. In this block, an instance should be
   # spawned and set up with anything project-specific.
