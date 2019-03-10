fabb550c17559badef76ae8cff7c124bafd24e8f
Adam Stankiewicz <sheerun@sher.pl>
2019-03-10 21:23:30 +0100

    Reuse server for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,7 @@ require 'vimrunner/rspec'
 $plugin_path = File.expand_path('../..', __FILE__)
 
 Vimrunner::RSpec.configure do |config|
-  # Use a single Vim instance for the test suite. Set to false to use an
-  # instance per test (slower, but can be easier to manage).
-  config.reuse_server = !ENV['REUSE_SERVER'].nil?
-
-  if Gem::Platform.local.os == "darwin"
-    config.reuse_server = true
-  end
+  config.reuse_server = true
 
   # Decide how to start a Vim instance. In this block, an instance should be
   # spawned and set up with anything project-specific.
