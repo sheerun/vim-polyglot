68f1e28e2ca6a58824d2d272aba971f90cb40d57
German Lashevich <german.lashevich@gmail.com>
2019-05-01 14:46:12 +0200

    Change approach for testing

---
+++ test/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+VIM="/bin/vim -c 'setrtp^=$(pwd)' -s test/test.vimscript"
+
+TEST_FILES=$(/bin/grep '^au' ftdetect/polyglot.vim \
+    | /bin/grep -E '\b(setfiletype|filetype|setf|ft)\b' \
+    | /usr/bin/awk '{print $3}' \
+    | /bin/grep -v / \
+    | /bin/sed -E -e 's/\[(.).+\]/\1/g' -e 's/\{([^,]+),[^}]+\}/\1/g' -e 's/\*/test/g' \
+    | /usr/bin/tr , '\n' \
+    | /bin/grep -iE '^[a-z\.]+$' \
+    | /usr/bin/sort -u)
+
+for f in $TEST_FILES;
+do
+    echo should parse $f file
+    touch /tmp/$f
+    $VIM /tmp/$f
+done
